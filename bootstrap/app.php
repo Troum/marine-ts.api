82684467d8be7cf7ee83b83d57cdcc3928df481e
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2026-04-15 22:27:24 +0300

    Update with architecture

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\SetApiLocale;
-use App\Http\Middleware\StripContentSecurityPolicyForTelescope;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,8 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(StripContentSecurityPolicyForTelescope::class);
-
         $middleware->appendToGroup('api', [
             SetApiLocale::class,
         ]);
